afbb35c7c4f38e4c8759a26e9d6613d2fbf22bf7
BlackBroke <paradox.ubivaet@mail.ru>
2022-06-24 17:42:14 +0800

    Добавьте файлы проекта.

---
+++ FirstWebProjectMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using FirstWebProjectMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebProjectMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else 
+                return View();
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ FirstWebProjectMVC/Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstWebProjectMVC.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
